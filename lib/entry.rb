8448c8ba86f0a5caad5c5df520608e80e56287d5
Sean Miller <sean@seanmiller.ca>
2015-07-18 14:39:08 -0400

    Add fix from entries for changing indent changing para

--- lib/entry.rb
+++ lib/entry.rb
@@ -12,8 +12,19 @@ class Entry
   def paragraphs
     return [] if @lines.count == 0
     trimmed(@lines).inject([[]]) do |result, line|
-      line.empty? ? result << [] : result.last << line
-      result
+      add_line_to_paragraphs(result, line)
     end
+  end
+
+  def add_line_to_paragraphs(result, line)
+    return (result << []) if line.empty?
+    return (result << [line]) if indent_changed?(line, result)
+    result.last << line
+    result
+  end
+
+  def indent_changed?(line, result)
+    return false if result.last.empty?
+    line.indent > 0 && (line.indent != result.last.last.indent)
   end
 end
